4ea3dadb81170daa3045d9501e9445d2c7deeaee
zetzer2626 <jnavarrete2626@gmail.com>
2025-01-27 17:52:02 -0300

    Descripción del cambio

--- portafolio/portafolio/settings.py
+++ portafolio/portafolio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'miapp',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
@@ -127,5 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'portafolio/static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ALLOWED_HOSTS = ['localhost','primer-portafolio-production.up.railway.app']
