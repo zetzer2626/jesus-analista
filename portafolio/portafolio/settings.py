deecd2c1bfce504e8bc9d145983696ec4f253475
zetzer2626 <jnavarrete2626@gmail.com>
2025-01-28 13:32:04 -0300

    prueba 2

--- portafolio/portafolio/settings.py
+++ portafolio/portafolio/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'miapp/static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
