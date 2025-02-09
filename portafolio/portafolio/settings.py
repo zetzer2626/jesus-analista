09f6f237e7c6762108b5e576586167f3f479b34a
zetzer2626 <jnavarrete2626@gmail.com>
2025-02-09 19:02:36 -0300

    co,it

--- portafolio/portafolio/settings.py
+++ portafolio/portafolio/settings.py
@@ -137,19 +137,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-
 # Archivos estáticos (CSS, JS)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Archivos de medios (imágenes, videos, etc.)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'miapp/static')
