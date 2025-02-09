ce3fbc0de0134d5fe42959296e58cde7d14bc2f8
zetzer2626 <jnavarrete2626@gmail.com>
2025-02-09 16:48:14 -0300

    prueba

--- portafolio/portafolio/settings.py
+++ portafolio/portafolio/settings.py
@@ -140,8 +140,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
